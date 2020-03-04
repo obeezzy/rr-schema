d890def79f1bb0e9622d81efeb009c863ac5b9c0
obeezzy <efeoghene.obebeduo@gmail.com>
2020-03-04 01:35:28 -0800

    Add ViewIncomeReport test

---
+++ tests/proctests/common/income/test_viewincomereport.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import unittest
+from proctests.utils import DatabaseClient, StoredProcedureTestCase, DatabaseResult, DatabaseDateTime
+from datetime import datetime, timedelta
+
+class ViewIncomeReport(StoredProcedureTestCase):
+    def test_view_income_report(self):
+        incomeTransaction1 = add_income_transaction(db=self.db,
+                                                        clientName="Jack Dorsey",
+                                                        purpose="Buy Twitter",
+                                                        amount=40)
+        incomeTransaction2 = add_income_transaction(db=self.db,
+                                                        clientName="Elon Musk",
+                                                        purpose="Buy Tesla",
+                                                        amount=90)
+        incomeTransaction3 = add_income_transaction(db=self.db,
+                                                        clientName="Mark Zuckerberg",
+                                                        purpose="Buy Facebook",
+                                                        amount=190)
+
+        today = datetime.now()
+        tomorrow = today + timedelta(days=1)
+        viewedIncomeReport = view_income_report(db=self.db,
+                                                        fromDateTime=today,
+                                                        toDateTime=tomorrow)
+
+        self.assertEqual(len(viewedIncomeReport), 3, "Expected 3 transactions.")
+        self.assertEqual(viewedIncomeReport[0]["income_transaction_id"], incomeTransaction1["income_transaction_id"], "Income transaction ID mismatch")
+        self.assertEqual(viewedIncomeReport[0]["purpose"], incomeTransaction1["purpose"], "Purpose mismatch")
+        self.assertEqual(viewedIncomeReport[0]["amount"], incomeTransaction1["amount"], "Amount mismatch")
+
+        self.assertEqual(viewedIncomeReport[1]["income_transaction_id"], incomeTransaction2["income_transaction_id"], "Income transaction ID mismatch")
+        self.assertEqual(viewedIncomeReport[1]["purpose"], incomeTransaction2["purpose"], "Purpose mismatch")
+        self.assertEqual(viewedIncomeReport[1]["amount"], incomeTransaction2["amount"], "Amount mismatch")
+
+        self.assertEqual(viewedIncomeReport[2]["income_transaction_id"], incomeTransaction3["income_transaction_id"], "Income transaction ID mismatch")
+        self.assertEqual(viewedIncomeReport[2]["purpose"], incomeTransaction3["purpose"], "Purpose mismatch")
+        self.assertEqual(viewedIncomeReport[2]["amount"], incomeTransaction3["amount"], "Amount mismatch")
+
+def add_income_transaction(db, clientName, purpose, amount):
+    incomeTransaction = {
+        "client_name": clientName,
+        "purpose": purpose,
+        "amount": amount,
+        "payment_method": "cash",
+        "currency": "NGN",
+        "user_id": 1
+    }
+
+    incomeTransactionTable = db.schema.get_table("income_transaction")
+    result = incomeTransactionTable.insert("client_name",
+                                            "purpose",
+                                            "amount",
+                                            "payment_method",
+                                            "currency",
+                                            "user_id") \
+                                    .values(tuple(incomeTransaction.values())) \
+                                    .execute()
+    incomeTransaction.update(DatabaseResult(result).fetch_one("income_transaction_id"))
+    return incomeTransaction
+
+def view_income_report(db, fromDateTime, toDateTime):
+    sqlResult = db.call_procedure("ViewIncomeReport", (
+                                    DatabaseDateTime(fromDateTime).iso_format,
+                                    DatabaseDateTime(toDateTime).iso_format))
+
+    return DatabaseResult(sqlResult).fetch_all()
+
+if __name__ == '__main__':
+    unittest.main()
