f09fd65f2e89877919520672a4b1eb6e91aad994
obeezzy <efeoghene.obebeduo@gmail.com>
2020-03-13 02:50:35 -0700

    Correct run script and update README

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,10 +2,10 @@
 
 if [ $# -ge 2 ]
 then
-    PYTHONPATH=. python3 -m unittest discover -s $1 -p $2 -v
+    python3 -m unittest discover -s $1 -p $2 -v
 elif [ $# -ge 1 ]
 then
-    PYTHONPATH=. python3 -m unittest discover -s $1 -v
+    python3 -m unittest discover -s $1 -v
 else
-    PYTHONPATH=. python3 -m unittest discover -s proctests -v
+    python3 -m unittest discover -s proctests -v
 fi
