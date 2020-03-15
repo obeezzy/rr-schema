4d7946d2ed0ac4af678f0ba7e723024f344b2ab5
obeezzy <efeoghene.obebeduo@gmail.com>
2020-03-15 03:01:04 -0700

    Add AddSoldProduct test
    - Remove unnecessary imports

--- tests/proctests/common/purchase/test_addpurchasedproduct.py
+++ tests/proctests/common/purchase/test_addpurchasedproduct.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from proctests.utils import StoredProcedureTestCase, DatabaseResult, DatabaseDateTime
-from datetime import datetime
+from proctests.utils import StoredProcedureTestCase, DatabaseResult
 
 class AddPurchasedProduct(StoredProcedureTestCase):
     def test_add_purchased_product(self):
